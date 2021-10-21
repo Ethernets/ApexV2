6029ad891f1d7a1e771a14c0fa833f1dc552b29c
Ethernets <ethernetss@gmail.com>
2021-10-21 14:32:15 +0300

    Add icons menu

--- app/src/main/java/com/example/apextracker/view/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/apextracker/view/activities/ProfileActivity.kt
@@ -3,10 +3,23 @@ package com.example.apextracker.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.apextracker.R
+import com.example.apextracker.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var myBinding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        myBinding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(myBinding.root)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(myBinding.toolbarProfile)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        myBinding.toolbarProfile.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/example/apextracker/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/apextracker/view/fragments/HomeFragment.kt
@@ -1,14 +1,15 @@
 package com.example.apextracker.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.apextracker.R
 import com.example.apextracker.databinding.FragmentHomeBinding
+import com.example.apextracker.view.activities.ProfileActivity
 import com.example.apextracker.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
@@ -19,6 +20,10 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +41,21 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_profile, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.action_profile -> {
+                startActivity(Intent(requireActivity(), ProfileActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
