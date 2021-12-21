8e8a5684920deecdcdc9863a9788cf2b1ebeee5c
Ethernets <ethernetss@gmail.com>
2021-12-21 09:23:14 +0200

    Unfortunately, due to the google policy, the YouTubeExtractor library is out of order.

--- app/src/main/java/com/example/apextracker/view/fragments/FunnyVideosFragment.kt
+++ app/src/main/java/com/example/apextracker/view/fragments/FunnyVideosFragment.kt
@@ -4,19 +4,38 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.util.SparseArray
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
+import at.huber.youtubeExtractor.VideoMeta
+import at.huber.youtubeExtractor.YouTubeExtractor
+import at.huber.youtubeExtractor.YtFile
 import com.example.apextracker.R
 import com.example.apextracker.databinding.FragmentFunnyVideosBinding
 import com.example.apextracker.view.activities.ProfileActivity
 import com.example.apextracker.viewmodel.FunnyVideosViewModel
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.MergingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.util.Util
 
 class FunnyVideosFragment : Fragment() {
 
     private lateinit var funnyVideosViewModel: FunnyVideosViewModel
+
     private var mBinding: FragmentFunnyVideosBinding? = null
+
+    private var playerExo: ExoPlayer? = null
+
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playBackPosition: Long = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,11 +65,69 @@ class FunnyVideosFragment : Fragment() {
 
     private fun infoYoutubeVideoObserver(){
         funnyVideosViewModel.loadInfoYoutubeVideoResponse.observe(viewLifecycleOwner){
-            val urlTest = "https://www.youtube.com/watch?v=ixvqwF4kpWQ"
+            val urlTest = "https://www.youtube.com/watch?v=MFfXkKp6jok"
             val uriTest: Uri = Uri.parse(urlTest)
-            mBinding?.videoView?.setVideoURI(uriTest)
-            mBinding?.videoView?.start()
+
+            initializePlayer(urlTest)
+           // mBinding?.videoView?.setVideoURI(uriTest)
+          //  mBinding?.videoView?.start()
             Log.i("Youtube", it.items.toString())
+        }
+    }
+
+    private fun initializePlayer(videoUrl: String){
+        playerExo = ExoPlayer.Builder(requireContext()).build()
+        mBinding?.videoView?.player = playerExo
+
+        object : YouTubeExtractor(requireContext()){
+            override fun onExtractionComplete(
+                ytFiles: SparseArray<YtFile>?,
+                videoMeta: VideoMeta?
+            ) {
+                if(ytFiles != null){
+                    val itag = 137 // Tag of video 1080p,
+                    val audioTag = 140 // Tag of m4a audio
+                    val youTubeUrl = ytFiles[itag].url
+                    val audioUrl = ytFiles[audioTag].url
+
+                    val audioSource: MediaSource = ProgressiveMediaSource
+                        .Factory(DefaultHttpDataSource.Factory())
+                        .createMediaSource(MediaItem.fromUri(audioUrl))
+                    val videoSource: MediaSource = ProgressiveMediaSource
+                        .Factory(DefaultHttpDataSource.Factory())
+                        .createMediaSource(MediaItem.fromUri(youTubeUrl))
+
+                    playerExo?.setMediaSource(MergingMediaSource(
+                        true, videoSource, audioSource
+                    ), true)
+                    playerExo?.prepare()
+                    playerExo?.playWhenReady = playWhenReady
+                    playerExo?.seekTo(currentWindow, playBackPosition)
+
+                }
+            }
+
+        }.extract(videoUrl, false, true)
+
+
+
+
+    }
+
+    private fun releasePlayer(){
+        if(playerExo != null){
+            playWhenReady = playerExo!!.playWhenReady
+            playBackPosition = playerExo!!.currentPosition
+            currentWindow = playerExo!!.currentMediaItemIndex
+            playerExo!!.release()
+            playerExo = null
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initializePlayer("https://www.youtube.com/watch?v=MFfXkKp6jok")
         }
     }
 
